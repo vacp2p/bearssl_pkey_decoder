546f8d9bb887ae1d8a23f62155c583acb0358046
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2023-01-15 10:47:01 +0100

    Removed header pragmas

--- bearssl_pkey_decoder.nim
+++ bearssl_pkey_decoder.nim
@@ -5,40 +5,38 @@ const
   bearPath = currentSourcePath.rsplit(DirSep, 1)[0] & DirSep &
              "bearssl_pkey_decoder" & DirSep & "csources"
 
-{.passC: "-I" & bearPath.}
-
-{.compile: bearPath & DirSep & "bearssl_pkey_decoder.c".}
+{.compile(bearPath & DirSep & "bearssl_pkey_decoder.c", "-I" & bearPath).}
 
 # This pragma should be the same as in nim-bearssl/decls.nim
 {.pragma: bearSslFunc, cdecl, gcsafe, noSideEffect, raises: [].}
 
 type
-  INNER_C_UNION_KEY* {.importc: "no_name", header: "bearssl_x509.h", bycopy, union.} = object
-    rsa* {.importc: "rsa".}: RsaPublicKey
-    ec* {.importc: "ec".}: EcPublicKey
+  INNER_C_UNION_KEY* {.union.} = object
+    rsa*: RsaPublicKey
+    ec*: EcPublicKey
 
-  INNER_C_STRUCT_CPU* {.importc: "no_name", header: "bearssl_x509.h", bycopy.} = object
-    dp* {.importc: "dp".}: ptr uint32
-    rp* {.importc: "rp".}: ptr uint32
-    ip* {.importc: "ip".}: ptr cuchar
+  INNER_C_STRUCT_CPU* = object
+    dp*: ptr uint32
+    rp*: ptr uint32
+    ip*: ptr cuchar
 
-  PkeyDecoderContext* {.importc: "br_pkey_decoder_context", header: "bearssl_pkey_decoder.h", bycopy.} = object
-    key* {.importc: "key".}: INNER_C_UNION_KEY
-    cpu* {.importc: "cpu".}: INNER_C_STRUCT_CPU
-    dpStack* {.importc: "dp_stack".}: array[32, uint32]
-    rpStack* {.importc: "rp_stack".}: array[32, uint32]
-    err* {.importc: "err".}: cint
-    hbuf* {.importc: "hbuf".}: pointer
+  PkeyDecoderContext* = object
+    key*: INNER_C_UNION_KEY
+    cpu*: INNER_C_STRUCT_CPU
+    dpStack*: array[32, uint32]
+    rpStack*: array[32, uint32]
+    err*: cint
+    hbuf*: pointer
+    hlen*: uint
     pad*: array[256, byte]
     key_type*: uint8
     key_data*: array[3 * X509_BUFSIZE_SIG, byte]
 
 proc pkeyDecoderInit*(ctx: ptr PkeyDecoderContext) {.bearSslFunc,
-    importc: "br_pkey_decoder_init", header: "bearssl_pkey_decoder.h".}
+                                importc: "br_pkey_decoder_init".}
 
 proc pkeyDecoderPush*(ctx: ptr PkeyDecoderContext; data: pointer; len: uint) {.bearSslFunc,
-    importc: "br_pkey_decoder_push", header: "bearssl_pkey_decoder.h".}
-
+                                                          importc: "br_pkey_decoder_push".}
 
 proc pkeyDecoderLastError*(ctx: ptr PkeyDecoderContext): cint =
   if ctx.err != 0: return ctx.err
